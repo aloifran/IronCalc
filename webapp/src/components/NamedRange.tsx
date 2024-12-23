33f6e21fd4eaf68f808d297145fc6d06b6dbe638
francisco aloi <francisco.aloi@klara.com>
2024-12-23 13:21:14 +0100

    NamedRange component changes

--- webapp/src/components/NamedRange.tsx
+++ webapp/src/components/NamedRange.tsx
@@ -1,97 +1,127 @@
-import type { Model, WorksheetProperties } from "@ironcalc/wasm";
-import { Box, IconButton, MenuItem, TextField, styled } from "@mui/material";
+import type { DefinedName, Model, WorksheetProperties } from "@ironcalc/wasm";
+import {
+  Box,
+  Divider,
+  IconButton,
+  MenuItem,
+  TextField,
+  styled,
+} from "@mui/material";
 import { Trash2 } from "lucide-react";
-
-export type NamedRangeObject = {
-  id: string;
-  name: string;
-  scope: string;
-  range: string;
-};
+import { useEffect, useState } from "react";
 
 type NamedRangeProperties = {
-  // come from model
-  id: string;
-  name: string;
-  scope: string;
-  range: string;
+  name?: string;
+  scope?: number;
+  formula: string;
   model: Model;
   worksheets: WorksheetProperties[];
-  // update namedRange in model
-  onChange: (id: string, field: string, value: string) => void;
+  onChange: (field: keyof DefinedName, value: string | number) => void;
+  onDelete?: () => void;
 };
 
 function NamedRange(props: NamedRangeProperties) {
-  // define onChange in parent for updating the model and values
+  const [canDelete, setCanDelete] = useState(false); // while creating deleted should be disabled
+  const [name, setName] = useState(props.name || "");
+  const [scope, setScope] = useState(props.scope || ""); // default should be Workbook (Global)
+  const [formula, setFormula] = useState(props.formula);
 
-  const handleDelete = () => {
-    // update model
-    console.log("deleted named range");
+  const handleChange = (field: keyof DefinedName, value: string) => {
+    if (field === "name") {
+      setName(value);
+      props.onChange("name", value);
+    }
+    if (field === "scope") {
+      setScope(value);
+      props.onChange("scope", value);
+    }
+    if (field === "formula") {
+      setFormula(value);
+      props.onChange("formula", value);
+    }
   };
 
+  // send unchanged formula value to parent
+  useEffect(() => {
+    handleChange("formula", formula);
+  });
+
+  //! add validations for name & range
   return (
-    <StyledBox>
-      {/* this is editable only while clicking */}
-      <TextField
-        id="name"
-        variant="outlined"
-        size="small"
-        margin="normal"
-        fullWidth
-        value={props.name}
-        onChange={(event) =>
-          props.onChange(props.id, "name", event.target.value)
-        }
-        onKeyDown={(event) => {
-          event.stopPropagation();
-        }}
-        onClick={(event) => event.stopPropagation()}
-      />
-      <TextField
-        id="scope"
-        variant="outlined"
-        select
-        defaultValue="Workbook"
-        size="small"
-        margin="normal"
-        fullWidth
-        value={props.scope}
-        onChange={(event) =>
-          props.onChange(props.id, "scope", event.target.value)
-        }
-      >
-        {props.worksheets.map((option) => (
-          <MenuItem key={option.sheet_id} value={option.name}>
-            {option.name}
-          </MenuItem>
-        ))}
-      </TextField>
-      <TextField
-        id="range"
-        variant="outlined"
-        size="small"
-        margin="normal"
-        fullWidth
-        value={props.range}
-        onChange={(event) =>
-          props.onChange(props.id, "range", event.target.value)
-        }
-        onKeyDown={(event) => {
-          event.stopPropagation();
-        }}
-        onClick={(event) => event.stopPropagation()}
-      />
-      {/* remove round hover animation  */}
-      <IconButton onClick={handleDelete}>
-        <Trash2 size={16} absoluteStrokeWidth />
-      </IconButton>
-    </StyledBox>
+    <>
+      <StyledBox>
+        <StyledTextField
+          id="name"
+          variant="outlined"
+          size="small"
+          margin="none"
+          fullWidth
+          value={name}
+          onChange={(event) => handleChange("name", event.target.value)}
+          onKeyDown={(event) => {
+            event.stopPropagation();
+          }}
+          onClick={(event) => event.stopPropagation()}
+        />
+        <StyledTextField
+          id="scope"
+          variant="outlined"
+          select
+          size="small"
+          margin="none"
+          fullWidth
+          value={scope}
+          onChange={
+            (event) => handleChange("scope", event.target.value) // send sheet_id
+          }
+        >
+          {props.worksheets.map((option) => (
+            <MenuItem key={option.sheet_id} value={option.sheet_id}>
+              {option.name}
+            </MenuItem>
+          ))}
+        </StyledTextField>
+        <StyledTextField
+          id="formula"
+          variant="outlined"
+          size="small"
+          margin="none"
+          fullWidth
+          value={formula}
+          onChange={(event) => handleChange("formula", event.target.value)}
+          onKeyDown={(event) => {
+            event.stopPropagation();
+          }}
+          onClick={(event) => event.stopPropagation()}
+        />
+        <StyledIconButton onClick={props.onDelete} disabled={canDelete}>
+          <Trash2 size={12} />
+        </StyledIconButton>
+      </StyledBox>
+      <Divider />
+    </>
   );
 }
 
 const StyledBox = styled(Box)`
 display: flex;
-gap: 15px;
+gap: 12px;
+width: 577px;
 `;
+
+const StyledTextField = styled(TextField)(() => ({
+  "& .MuiInputBase-root": {
+    height: "28px",
+    margin: 0,
+  },
+}));
+
+const StyledIconButton = styled(IconButton)(({ theme }) => ({
+  color: theme.palette.error.main,
+  "&.Mui-disabled": {
+    opacity: 0.6,
+    color: theme.palette.error.light,
+  },
+}));
 
 export default NamedRange;
